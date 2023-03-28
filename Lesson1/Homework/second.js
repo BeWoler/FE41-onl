a1754ee4046aa68225deabad383cee6d07dcfb9b
BeWoler <bewoler.del@gmail.com>
2023-03-29 01:22:50 +0300

    added homeworks & first lesson

---
+++ Lesson1/Homework/second.js
@@ -0,0 +1,20 @@
+// Что выведет в консоль?
+
+// Просьба подумать и сразу не запускать для проверки :)
+// Ответ просто запишите комментарием под вызовом функции
+
+const log = () => {
+  console.log(1)
+
+  setTimeout(() => { console.log(2) }, 0)
+
+  console.log(3)
+  
+  new Promise(() => { console.log(4) })
+  
+  setTimeout(() => { console.log(5) }, 1000)
+
+  console.log(6)
+}
+
+log()
