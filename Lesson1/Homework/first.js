b58fee67985d7396128f9c5d2e8ba052b415b41a
Anton-Ostrovskiy <ostrovskiy.98@mail.ru>
2023-04-03 23:17:22 +0300

    HW-1

--- Lesson1/Homework/first.js
+++ Lesson1/Homework/first.js
@@ -22,3 +22,49 @@ const subjects = {
       teachers: 3
   }
 }
+
+// #1
+
+const subjectsKeys = Object.keys(subjects);
+console.log(subjectsKeys);
+const subjectsToString = String(subjectsKeys)
+
+console.log(subjectsToString);
+
+// #2
+
+const objectToArray = Object.values(subjects);
+console.log(objectToArray);
+
+const numStudents = (arr) => {
+    const sum = arr.reduce((sum, currValue) => {
+      return sum + currValue.students;
+    }, 0);
+    return sum;
+}
+
+const numTeachers = (arr) => {
+    const sum = arr.reduce((sum, currValue) => {
+      return sum + currValue.teachers;
+    }, 0);
+    return sum;
+}
+
+const totalNumStudents = numStudents(objectToArray);
+const totalNumTeachers = numTeachers(objectToArray);
+
+console.log("Студентов:", totalNumStudents);
+console.log("Учителей:", totalNumTeachers);
+
+// #3
+
+const averageStudents = totalNumStudents / objectToArray.length;
+
+console.log(averageStudents);
+
+  // #5
+
+
+const sortedTeachers = objectToArray.sort((a, b) => b.teachers - a.teachers);
+  console.log(sortedTeachers);
+
