3cc92492c1879018f00777ab6200d0a5a52defb6
akavaliova <kovaleva89alena@gmail.com>
2023-04-07 15:54:08 +0200

    Typed the data

---
+++ a_kavaliova-hw_lesson_3/dist/src/a_kavaliova-hw_lesson_3.ts
@@ -0,0 +1,82 @@
+// Установить тс с нуля (описание в index.ts)
+// Протипизировать данные
+// По возможности использовать интерфейсы при необходимости
+
+
+// 1
+
+interface IObjFirst {
+    name: string;
+    years: number;
+}
+
+const obj: IObjFirst = {
+    name: "Any name",
+    years: 20,
+  };
+  
+  // 2
+  
+  const numSum = (a: number, b: number): number => {
+    return a + b;
+  };
+  
+  // 3
+  
+  const anyFunc = (): any => {
+    return anyFunc();
+  };
+  
+  // 4
+  
+  const newArr: number[] = [1, 2, 3, 4, 5, 6, 7, 8];
+  
+  const diffArr: (number | string | boolean)[] = [1, "Hello", true];
+
+  interface IPerson {
+    id: number;
+    name: string;
+    years: number;
+  }
+  
+  const objArr: IPerson[] = [
+    { id: 1, name: "Sasha", years: 30 },
+    { id: 2, name: "Roma", years: 18 },
+    { id: 3, name: "Misha", years: 23 },
+  ];
+  
+  
+  // 5
+
+  interface IProducts {
+    id: number;
+    name: string;
+    price: number;
+    currency: string;
+    ingredients: string[];
+    type: string;
+    available: boolean;
+  }
+  
+  const products: IProducts[] = [
+    {
+      id: 1,
+      name: "Burger Premium",
+      price: 6,
+      currency: "euro",
+      ingredients: ["flour", "beef", "salad", "cheese", "sauce"],
+      type: "burger",
+      available: true,
+    },
+    {
+      id: 2,
+      name: "Burger Lite",
+      price: 2,
+      currency: "euro",
+      ingredients: ["flour", "beef", "cheese", "sauce", "cucumber"],
+      type: "burger",
+      available: true,
+    },
+  ];
+  
+  
