65fc6bb99a64a06b66190c1ef8b325cd2d592031
LoKyLj <kyljs3d@gmail.com>
2023-04-11 09:22:44 +0300

    Finally commited

--- Lesson1/Homework/first.js
+++ Lesson1/Homework/first.js
@@ -5,20 +5,76 @@
 // 5. Получить массив из предметов и отсортировать по количеству преподавателей на факультете от большего к меньшему
 
 const subjects = {
-  mathematics: {
-      students: 200,
-      teachers: 6
-  },
-  biology: {
-      students: 120,
-      teachers: 6
-  },
-  geography: {
-      students: 60,
-      teachers: 2
-  },
-  chemistry: {
-      students: 100,
-      teachers: 3
-  }
+    mathematics: {
+        students: 200,
+        teachers: 6
+    },
+    biology: {
+        students: 120,
+        teachers: 6
+    },
+    geography: {
+        students: 60,   
+        teachers: 2
+    },
+    chemistry: {
+        students: 100,
+        teachers: 3
+    }
 }
+
+// Task 1
+
+const subjectsKeys = Object.keys(subjects); // (4) ['mathematics', 'biology', 'geography', 'chemistry']
+
+const subjectsNamesString = `Subjects names are: ${subjectsKeys.join(', ')}`; // Subjects names are: mathematics, biology, geography, chemistry
+
+// Task 2
+
+const subjectsValues = Object.values(subjects);  // (4) [{…}, {…}, {…}, {…}]
+
+let commonPeopleNumber = 0;
+
+subjectsValues.forEach(subj => {
+    commonPeopleNumber = commonPeopleNumber + subj.students + subj.teachers
+}); // 497
+
+// Task 3
+
+let commonStudentNumber = 0;
+
+subjectsValues.forEach(subj => {
+    commonStudentNumber += subj.students;
+})
+
+const averageStudentsNumber = commonStudentNumber / subjectsValues.length; // 120
+
+// Task 4
+
+const subjectsNames = Object.keys(subjects); // (4) ['mathematics', 'biology', 'geography', 'chemistry']
+const subjectsObjToArr = subjectsNames.map((subject) => {
+    return {
+        name: subject,
+        students: subjects[subject].students,
+        teachers: subjects[subject].teachers
+    };
+});
+
+subjectsObjToArr;  
+// [{…}, {…}, {…}, {…}]
+// 0: {name: 'mathematics', students: 200, teachers: 6}
+// 1: {name: 'biology', students: 120, teachers: 6}
+// 2: {name: 'geography',     students: 60, teachers: 2}
+// 3: {name: 'chemistry', students: 100, teachers: 3}
+
+
+// Task 5
+
+const sortedByTeachers = subjectsObjToArr.sort((a, b) => b.teachers - a.teachers);
+
+sortedByTeachers; 
+// [{…}, {…}, {…}, {…}]
+// 0: {name: 'mathematics', students: 200, teachers: 6}
+// 1: {name: 'biology', students: 120, teachers: 6}
+// 2: {name: 'chemistry', students: 100, teachers: 3}
+// 3: {name: 'geography', students: 60, teachers: 2}
--- Lesson1/Homework/third.js
+++ Lesson1/Homework/third.js
@@ -1,9 +1,8 @@
 // Написать функцию, которая принимает на вход строку в виде чисел и возвращает минимальное и максимальное значение.
 
 const findMinAndMax = (numbers) => {
-
+    const arr = numbers.split(' ')
+    return Math.min(...arr) + ' ' + Math.max(...arr)
 }
 
-findMinAndMax('5 6 3 0 1 -100')
-
-console.log(findMinAndMax('5 6 3 0 1 -100')) // должен вывести -100 и 6
+console.log(findMinAndMax('5 6 3 0 1 -100'))
