397e54f3eab8e47fd08481d2a428937c09bcdba1
karinayatchenko <karina.yatchenko@hellofresh.com>
2023-04-11 17:19:30 -0400

    create karinayatchenko and added hw1 and hw3

---
+++ Homework/hw3/homework_3.js
@@ -0,0 +1,42 @@
+"use strict";
+// 1
+const object = {
+    name: "Any name",
+    years: 20,
+};
+// 2
+const numberSum = (a, b) => {
+    return a + b;
+};
+// 3
+const anyFunction = () => {
+    return anyFunction();
+};
+// 4
+const newArray = [1, 2, 3, 4, 5, 6, 7, 8];
+const diffArray = [1, "Hello", true];
+const objArray = [
+    { id: 1, name: "Sasha", years: 30 },
+    { id: 2, name: "Roma", years: 18 },
+    { id: 3, name: "Misha", years: 23 },
+];
+const productsObject = [
+    {
+        id: 1,
+        name: "Burger Premium",
+        price: 6,
+        currency: "euro",
+        ingredients: ["flour", "beef", "salad", "cheese", "sauce"],
+        type: "burger",
+        available: true,
+    },
+    {
+        id: 2,
+        name: "Burger Lite",
+        price: 2,
+        currency: "euro",
+        ingredients: ["flour", "beef", "cheese", "sauce", "cucumber"],
+        type: "burger",
+        available: true,
+    },
+];
