c789bb03a8bc5a74bb8ae93f5cceac83ca0f36ea
VladimirBashylau <vladimirbvv01@yandex.ru>
2023-04-24 20:40:30 +0300

    HW-5

---
+++ react_hw/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './components/Header/Header';
+
+
+function App() {
+  return (
+    <div>
+      <Header isAuth = {true} />
+    </div>
+  );
+}
+
+export default App;
