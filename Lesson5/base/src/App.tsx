c2d7fe4bce0b1923b01dd24c593546d01ebe7233
GrishaPastukhov <grishafox505@gmail.com>
2023-04-28 13:15:26 +0300

    react hw1 test some functions with confirm

--- Lesson5/base/src/App.tsx
+++ Lesson5/base/src/App.tsx
@@ -1,10 +1,14 @@
 import { resourceLimits } from "worker_threads";
 import Header from "./components/Header";
 
+const Func = () => {
+    let torf = window.confirm('t or f');
+    return (torf);
+}
 const App = () => {
     return(
         <div>
-            <Header isAuth={true}/>
+            <Header isAuth={Func()}/>
         </div>
     )
 }
