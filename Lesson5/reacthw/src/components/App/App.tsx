fc352bb90e8123950aca0570367f092034f351a4
unknown <ostrovskiy.98@mail.ru>
2023-05-01 15:43:46 +0300

    add HW-6

--- Lesson5/reacthw/src/components/App/App.tsx
+++ Lesson5/reacthw/src/components/App/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../Header/Header';
+import MainPage from '../MainPage/MainPage';
 
 function App() {
   return (
     <div>
-      <Header isAuth = {false} />
+      <Header isAuth = {true} />
+      <MainPage/>
     </div>
   );
 }
