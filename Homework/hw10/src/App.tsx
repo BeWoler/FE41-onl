5271287afc41d55585ad65914fff2419bd03f912
karinayatchenko <karina.yatchenko@hellofresh.com>
2023-05-22 19:37:37 -0400

    hw10-routes

---
+++ Homework/hw10/src/App.tsx
@@ -0,0 +1,15 @@
+import AppRouter from './components/AppRouter/AppRouter';
+import Nav from './components/Nav/Nav';
+
+
+function App() {
+  return (
+    <div>
+      <Nav />
+      <AppRouter />
+    </div>
+
+  );
+}
+
+export default App;
---
+++ Homework/hw10/src/components/PostsPage/PostsPage.tsx
@@ -0,0 +1,12 @@
+import PostList from '../PostList/PostList'
+
+const PostsPage = () => {
+
+    return (
+        <div>
+            <PostList />
+        </div>
+    )
+}
+
+export default PostsPage
